d0d4e8965a22163670ce387f7ec619040b3e2696
ESKYGROUP\d.pozimski <damian.pozimski@esky.com>
2018-09-08 09:35:45 +0200

    App insights

--- RequestLogger/Program.cs
+++ RequestLogger/Program.cs
@@ -19,6 +19,7 @@ namespace RequestLogger
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
